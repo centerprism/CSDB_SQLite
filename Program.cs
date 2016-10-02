dd181d85685245a9901d959225d9604d2aac4f83
centerprism <cunha.luisprata@gmail.com>
2016-10-02 17:18:49 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+using CSharp.Introduction;  
+using MessageSource = CSharp.Introduction.HelloMessage;  
+namespace delagate
+{
+	enum Color { Red, Blue, Green }
+	class MainClass
+
+	{
+		delegate void SimpleDelegate();
+
+		static void F() {  
+			System.Console.WriteLine("HOLA");  
+		}  
+		public static void Main(string[] args)
+		{
+			SimpleDelegate d = new SimpleDelegate(F);  
+			d(); 
+			F ();
+
+			MessageSource m = new MessageSource();  
+			System.Console.WriteLine(m.Message); 
+		}
+
+	}
+}
+
+// HelloLibrary.cs  
+namespace CSharp.Introduction  
+{  
+	public class HelloMessage  
+	{  
+		public string Message {  
+			get {  
+				return "hello, world";  
+			}  
+		}  
+	}  
+}  
